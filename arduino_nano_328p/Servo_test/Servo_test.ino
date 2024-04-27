5f854bfe3ac6cfa62e5dfbfcda20dcee72e371f8
Vlad <Klisunov.vladdi94@gmail.com>
2024-04-27 18:22:10 +0300

    ...

--- arduino_nano_328p/Servo_test/Servo_test.ino
+++ arduino_nano_328p/Servo_test/Servo_test.ino
@@ -1,9 +1,38 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "Servo.h"
+ 
+#define SERVO1_PIN 9
 
+int angle=0;
+ 
+Servo Servo1;
+ 
+void setup()
+{  
+  Servo1.attach(SERVO1_PIN);
+  Serial.begin(9600);  
 }
+ 
+void loop()
+{ 
+  Servo1.write(angle); 
+  delay(10);
+  angle++;
+  if(angle>180){
+    angle=0;
+    Servo1.write(angle);
+    delay(1000);
+  }
+  
+  
 
-void loop() {
-  // put your main code here, to run repeatedly:
+  if(Serial.available()){
+    String received_string=Serial.readString();  
+    int angle=received_string.toInt();
+    Servo1.write(angle);
+    Serial.print(angle);Serial.print(" - ");
+    Serial.println("ok");
 
+  }
+  
 }
+ 
