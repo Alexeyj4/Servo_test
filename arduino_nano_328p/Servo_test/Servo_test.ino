9d7ad08a42f218fc465c40d53a0b0238dfbf0aea
Vlad <Klisunov.vladdi94@gmail.com>
2024-05-14 15:19:56 +0300

    Update Servo_test.ino

--- arduino_nano_328p/Servo_test/Servo_test.ino
+++ arduino_nano_328p/Servo_test/Servo_test.ino
@@ -1,15 +1,18 @@
 #include "Servo.h"
  
 #define SERVO1_PIN 9
+#define SERVO2_PIN 10
 
 int angle=0;
  
 Servo Servo1;
+Servo Servo2;
  
 void setup()
 {  
   Servo1.attach(SERVO1_PIN);
-  Serial.begin(9600);  
+  Servo2.attach(SERVO2_PIN);
+  Serial.begin(115200);  
 }
  
 void loop()
@@ -18,9 +21,10 @@ void loop()
 
   if(Serial.available()){
     String received_string=Serial.readString();  
-    int angle=received_string.toInt();
-    Servo1.write(angle);
-    Serial.print(angle);Serial.print(" - ");
+    int angle1=received_string.toInt();
+    int angle2=180-received_string.toInt();
+    Servo1.write(angle1);
+    Servo2.write(angle2);    
     Serial.println("ok");
 
   }
